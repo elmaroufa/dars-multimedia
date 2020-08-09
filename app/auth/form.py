19d1fd9cf83788eec7a88641a704727e7e52dd54
Saly Abbo <elmaroufa@gmail.com>
2020-08-09 23:40:09 +0100

    Ajout des inputdu model media dans le fichier form de l application

--- app/auth/form.py
+++ app/auth/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from ..models import User
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, ValidationError
 from wtforms.validators import DataRequired, Length, Email,Regexp,EqualTo
 
 class LoginForm(FlaskForm):
@@ -18,7 +18,7 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_email(self,field):
-         if User.query.filter_by(email=filter.data).first():
+         if User.query.filter_by(email=field.data).first():
              raise ValidationError("email alredy register")
 
     def validate_username(self,field):
@@ -26,16 +26,21 @@ class RegisterForm(FlaskForm):
             raise ValidationError('user already using')
 
 class PrediForm(FlaskForm):
-    name = StringField('Nom Predicateur', validators=[DataRequired(), Length(1, 64)])
-    language = StringField('Langue predication', validators=[DataRequired(), Length(1, 64)])
-    description = StringField('Description', validators=[DataRequired(), Length(1, 64)])
-    city = StringField('Ville du predicateur', validators=[DataRequired(), Length(1, 64)])
-    info_youtube = StringField('Lien sur youtube', validators=[DataRequired(), Length(1, 64)])
-    info_telegram = StringField('Lien telegram', validators=[DataRequired(), Length(1, 64)])
+    name = StringField('Nom Predicateur', validators=[DataRequired(), Length(min=1, max=64)])
+    language = StringField('Langue predication', validators=[DataRequired(), Length(min=1, max=64)])
+    description = StringField('Description', validators=[DataRequired(), Length(min=1, max=64)])
+    city = StringField('Ville du predicateur', validators=[DataRequired(), Length(min=1, max=64)])
+    info_youtube = StringField('Lien sur youtube', validators=[DataRequired(), Length(min=1, max=64)])
+    info_telegram = StringField('Lien telegram', validators=[DataRequired(), Length(min=1, max=64)])
 
 
 class MultimediForm(FlaskForm):
-    title = StringField('Titre du media', validators=[DataRequired(), Length(1, 64)])
+    title = StringField('Titre du media', validators=[DataRequired(), Length(min=1, max=64)])
+    type_media = StringField('Type media', validators=[DataRequired(), Length(min=1, max=64)])
+    theme = StringField('Theme du media', validators=[DataRequired(), Length(min=1, max=64)])
+    body_iframe= TextAreaField('Html du media', validators=[DataRequired(), Length(min=1, max=500)])
+    link_dowload = TextAreaField('Telechargement du media', validators=[DataRequired(), Length(min=1, max=500)])
+
 
 class NewsletterForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(min=1, max=64), Email()])
