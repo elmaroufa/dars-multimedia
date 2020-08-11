0751f7d9d048b3405db02ea1dcc914491d0f3d05
Saly Abbo <elmaroufa@gmail.com>
2020-08-11 16:12:00 +0100

    fin du travail sur le crud du model predicateur

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,19 @@ from .form import LoginForm, RegisterForm, PrediForm
 from ..models import User, Predicateur, Multimedia, Newsletter
 from app import db
 
+'''
+fonction add request form predicateur
+'''
+def idratePredicateur(predicateur):
+    predicateur.name = request.form['name_predi']
+    predicateur.language = request.form['language_predi']
+    predicateur.descriptions = request.form['descr_predi']
+    predicateur.city = request.form['city_predi']
+    predicateur.info_youtube = request.form['youtube_predi']
+    predicateur.info_telegram = request.form['telegram_predi']
+    predicateur.author_id = request.form['user_id']
+    return predicateur
+
 @auth.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
@@ -47,14 +60,8 @@ def all_predicateur():
 def add_predicateur():
     predicateur = Predicateur()
     if request.method == 'POST':
-        predicateur.name = request.form['name_predi']
-        predicateur.language = request.form['language_predi']
-        predicateur.descriptions = request.form['descr_predi']
-        predicateur.city = request.form['city_predi']
-        predicateur.info_youtube = request.form['youtube_predi']
-        predicateur.info_telegram = request.form['telegram_predi']
-        predicateur.author_id = request.form['user_id']
-        db.session.add(predicateur)
+        predi = idratePredicateur(predicateur)
+        db.session.add(predi)
         db.session.commit()
         flash('Creation de predicateur reussi', 'success')
     return render_template('add_predicateur.html')
@@ -66,12 +73,29 @@ def add_predicateur():
 def update_predicateur(predi_id):
     predicateur = Predicateur.query.get_or_404(predi_id)
     formpredi = PrediForm()
-    formpredi.name.data = predicateur.name
-    formpredi.language.data = predicateur.language
-    formpredi.city.data = predicateur.city
-    formpredi.info_telegram.data =  predicateur.info_telegram
-    formpredi.info_youtube.data = predicateur.youtube_predi
-    formpredi.description.data = predicateur.description
+    if request.method == 'POST':
+        predicateur = idratePredicateur(predicateur)
+        db.session.commit()
+        flash('Modificateur terminer','success')
+        return redirect(url_for('auth.all_predicateur'))
+    return render_template('update_predi.html',predicateur=predicateur,formpredi=formpredi)
+
+'''
+delete predicateur
+'''
+@auth.route('/admin/predicateur/delete/<int:predi_id>')
+@login_required
+def delete_predi(predi_id):
+    predicateur = Predicateur.query.get_or_404(predi_id)
+    media = Multimedia.query.filter_by(predicateur_id=predi_id)
+    db.session.delete(predicateur)
+    db.session.delete(media)
+    db.session.commit()
+    flash('supression predicateur reussi','succes')
+    return redirect(url_for('auth.all_predicateur'))
+
+
+
 
 
 
