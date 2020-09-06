b051c44d993f4de67f76ea799a90123e2d209873
Saly Abbo <elmaroufa@gmail.com>
2020-09-06 11:05:54 +0100

    creation des routes pour voir les medias cours,sermont,conferences,listes des predicateurs'

--- app/main/views.py
+++ app/main/views.py
@@ -29,7 +29,7 @@ def index():
     lastSermont.reverse()
     lastConference.reverse()
     last_course.reverse()
-    return render_template('main.html',last_course=last_course,predicateurs=predicateurs, \
+    return render_template('main.html',last_course=last_course,predicateurs=predicateurs,\
         lastConference=lastConference,lastSermont = lastSermont)
 
 
@@ -41,3 +41,32 @@ def allMedias(type_media):
     medias = Multimedia.query.filter_by(type_media=type_media).all()
     medias.reverse()
     return render_template('medias.html',medias=medias)
+
+@main.route('/medias/course')
+def allCourses():
+    medias = Multimedia.query.filter_by(type_media='COURSES',index_media=1).all()
+    medias.reverse()
+    return render_template('course.html',courses=medias)
+
+@main.route('/medias/chapitre/<string:code_course>')
+def allChapitre(code_course):
+    medias = Multimedia.query.filter_by(code_course=code_course).all()
+    return render_template('chapitre.html',medias=medias)
+
+@main.route('/medias/<int:id_media>')
+def viewMedia(id_media):
+    media = Multimedia.query.filter_by(id=id_media).first()
+    return render_template('view_media.html',media=media)
+
+@main.route('/predicateurs')
+def allpredicateur():
+    predicateur = Predicateur.query.all()
+    return render_template('predicateur.html',predicateur=predicateur)
+
+@main.route('/predicateur/media/<int:id_predicateur>')
+def mediaPredicateur(id_predicateur):
+    medias = Multimedia.query.filter_by(predicateur_id=id_predicateur,\
+        index_media=0).all()
+    courses = Multimedia.query.filter_by(predicateur_id = id_predicateur,\
+        index_media=1).all()
+    return render_template('media_predicateur',medias=medias,courses=courses)
